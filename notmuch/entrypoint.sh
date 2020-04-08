1144c7fb42686bf688d3d667904f807ab48f62d7
Tim Quelch <tim@quelch.name>
2020-04-08 18:54:47 +1000

    Only copy dir if it is mounted

--- notmuch/entrypoint.sh
+++ notmuch/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 set -e
 
-cp -R /tmp/.ssh /var/mail/.ssh
-chown -R mail:mail /var/mail/.ssh
-chmod 700 /var/mail/.ssh
-chmod 600 /var/mail/.ssh/authorized_keys
 # Generate ssh hostkeys
 ssh-keygen -A
 
+# Copy mounted ssh dir to userdir so we can access
+if [ -d /tmp/.ssh ]; then
+    cp -R /tmp/.ssh /var/mail/.ssh
+    chown -R mail:mail /var/mail/.ssh
+    chmod 700 /var/mail/.ssh
+    chmod 600 /var/mail/.ssh/authorized_keys
+fi
 
 exec "$@"

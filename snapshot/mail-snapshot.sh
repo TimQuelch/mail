0335a000576c1eec4b1d5701959640c20628be32
Tim <tim@tquelch.com>
2020-11-16 04:11:44 +0000

    Add script and unit files to backup regularly

---
+++ snapshot/mail-snapshot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mnt=/mnt/mail
+maildir="$mnt/maildir"
+exclude=".notmuch"
+bucket="s3://timquelch-mail-archive"
+
+cd $maildir || exit
+template="$mnt/maildir-$1-$(date -Iminutes)-XXXXX.tar.gz"
+afile="$(mktemp $template)"
+
+echo "Creating maildir snapshot: $afile"
+tar -czf "$afile" --exclude "$exclude" *
+
+echo "Moving $1 snapshot to $bucket storage"
+aws s3 mv "$afile" "$bucket" --no-progress --storage-class STANDARD_IA
+
+echo "Completed $1 snapshot"
